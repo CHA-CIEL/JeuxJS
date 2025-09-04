9136cf9fd514b747fa78446112097b4343f6a88c
joris chaintron <jorischaintron69@gmail.com>
2025-09-04 11:10:42 +0200

    #2  Création du serveur Web en Node.js

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 'use strict';
 
 console.log('TP CIEL');
+
+/* *********************** Serveur Web ************************ */
+
+var port = 80;
+var express = require('express');
+var app = express(); 
+
+app.use(express.static(__dirname + '/www'));
+
+// Route principale
+app.get('/', function (req, res) {
+    console.log('Réponse à un client');
+    res.sendFile(__dirname + '/www/index.html');
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Erreur serveur express');
+});
+
+app.listen(port, function () {
+    console.log('Serveur en écoute sur le port ' + port);
+});
